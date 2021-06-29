ffdde80d312b814d739349ab3555de7ca6e1cf73
Gromenawuer <gromenawuer12@gmail.com>
2021-06-29 18:02:55 +0200

    Fixing docker build and preparing transition to AWS

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask_lambda import FlaskLambda
 from .users.infrastructure.api.users_blueprint import create_users_blueprint
 from .config import configure_inject
 
-def create_app() -> Flask:
-    app = Flask(__name__)
+def create_app() -> FlaskLambda:
+    app = FlaskLambda(__name__)
     configure_inject(app)
 
     app.register_blueprint(create_users_blueprint(), url_prefix='/api')
