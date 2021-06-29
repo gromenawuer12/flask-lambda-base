2ed5fa866ffd005eeb6efbcbf9fd32e8e31bc217
Gromenawuer <gromenawuer12@gmail.com>
2021-06-29 18:14:37 +0200

    Fixing reviewers comments

--- src/app.py
+++ src/app.py
@@ -1,11 +1,10 @@
 from flask_lambda import FlaskLambda
-from .users.infrastructure.api.users_blueprint import create_users_blueprint
-from .config import configure_inject
+from users.infrastructure.api.users_blueprint import create_users_blueprint
+from config import configure_inject
 
-def create_app() -> FlaskLambda:
-    app = FlaskLambda(__name__)
-    configure_inject(app)
+app = FlaskLambda(__name__)
+configure_inject(app)
 
-    app.register_blueprint(create_users_blueprint(), url_prefix='/api')
+app.register_blueprint(create_users_blueprint(), url_prefix='/api')
 
-    return app
+    
--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import inject
-from .users.domain.user_database import UserDatabase
-from .users.infrastructure.database.user_in_memory import UserInMemory
+from users.domain.user_database import UserDatabase
+from users.infrastructure.database.user_in_memory import UserInMemory
 
 def configure_inject(app: Flask) -> None:
     def config(binder: inject.Binder) -> None:
