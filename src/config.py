ccc00bad08e286e98a68f1f023633d35bc6ebe0a
Gromenawuer <gromenawuer12@gmail.com>
2021-07-16 13:23:32 +0200

    Adding dynamodb

--- src/config.py
+++ src/config.py
@@ -1,10 +1,10 @@
 from flask import Flask
 import inject
 from users.domain.user_database import UserDatabase
-from users.infrastructure.database.user_in_memory import UserInMemory
+from users.infrastructure.database.user_dynamodb import UserDynamoDB
 
 def configure_inject(app: Flask) -> None:
     def config(binder: inject.Binder) -> None:
-        binder.bind(UserDatabase, UserInMemory())
+        binder.bind(UserDatabase, UserDynamoDB())
 
     inject.configure(config)
